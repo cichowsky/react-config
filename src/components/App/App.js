8733b13ccb5794640d2086855a43909209db27e3
Michal 'Cichy' Czubak <czubakmichal@o2.pl>
2020-02-10 19:51:09 +0100

    add project config: absolute imports, eslint(airbnb), prettier, husky and lint-staged

---
+++ src/components/App/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+      <p>Project config: absolute imports, eslint (airbnb), prettier, husky and lint-staged</p>
+    </div>
+  );
+};
+
+export default App;
